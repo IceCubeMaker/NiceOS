22832552389f4c97dc5eabbb257c1b82af66be94
franz <franzluehr@gmail.com>
2026-04-15 17:41:53 +0200

    made update available through terminal

--- core/scripts/scripts-importer.nix
+++ core/scripts/scripts-importer.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 let
-  tipsFile     = pkgs.writeTextFile { name = "tips.sh"; text = builtins.readFile ./tips.sh; destination = "/share/niceos/tips.sh"; };
+  tipsFile      = pkgs.writeTextFile { name = "tips.sh"; text = builtins.readFile ./tips.sh; destination = "/share/niceos/tips.sh"; };
   rebuildScript = pkgs.writeShellScriptBin "rebuild" (builtins.readFile ./rebuild.sh);
   installScript = pkgs.writeShellScriptBin "niceos-install" (builtins.readFile ./install.sh);
+  updateScript  = pkgs.writeShellScriptBin "update" (builtins.readFile ./update.sh);
 in {
-  environment.systemPackages = [ tipsFile rebuildScript installScript ];
+  environment.systemPackages = [ tipsFile rebuildScript installScript updateScript ];
 }
